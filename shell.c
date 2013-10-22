e71e6273e8c04fed070a7f5fdf05ad418ac65c55
lixianxie <lxie@smu.edu>
2013-10-22 01:03:46 -0500

    implement exit function

--- shell.c
+++ shell.c
@@ -3,10 +3,40 @@
 
 
 #include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 
-int main(void) {
+void error();
+bool cmdIsExit(char* command);
+int cmdIsTyepe();
+int cmdIsCopy();
+int cmdIsDelete();
+int cmdIsExecutable();
+void type();
+char* filenname();
+char* filename();
+void copy();
+void delete();
+void excute();
+
+
+int main(int argc, char** argv) {
 	printf("hello world!\n");
-	char command[100]={0};
-	scanf("%s",command);
+	char command[100];
+        while(true){
+		printf("> ");
+		scanf("%s",command);
+		if(cmdIsExit(command)){
+			break;			
+			}
+
+	}
 	return 0;
 }
+
+
+bool cmdIsExit(char* command){
+	return !strcmp(command,"exit");
+	}
